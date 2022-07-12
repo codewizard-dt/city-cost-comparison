44ad05ef8b764c45e679fb74844730067571a209
David Taylor <94571813+codewizard-dt@users.noreply.github.com>
2022-07-12 06:54:39 -0500

    MapApi combining GeoApi, CostApi and Google Maps

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
    * Checks cache and loads recently searched cities
    */
   GeoApi.renderCachedResults()
-
+  CostApi.fetchCityList().then(() => CostApi.loading = false)
 });
 
 
@@ -30,7 +30,7 @@ const GeoApi = {
   },
   fetchOptions: {
     headers: {
-      'X-RapidAPI-Key': '06360e6be9msha9eb1136e2ae6afp1680b5jsn2f79a3fd3619',
+      'X-RapidAPI-Key': '125fc260f4mshd4fee63046143a4p123388jsn99b684846699',
       'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
     }
   },
@@ -50,6 +50,7 @@ const GeoApi = {
       const { data } = await res.json()
       this.results = data
       console.log(this, this.results)
+      return data
     } catch (err) {
       console.log(err)
       return { error: err }
@@ -72,6 +73,7 @@ const GeoApi = {
     localStorage.setItem('GeoApiCache', JSON.stringify(cities))
   },
   cacheToLocal: function (city) {
+    if (!city.name) throw { error: 'no city name', city }
     let stored = this.cache.filter(cached => cached.id !== city.id)
     stored.unshift(city)
     this.cache = stored
@@ -96,8 +98,10 @@ const GeoApi = {
         if (costData.error) {
           // TODO: ERROR HANDLING
         } else {
+          let url = document.location.href
           localStorage.setItem('city_id', costData.city_id)
-          document.location.href = 'results.html'
+          if (!url.includes('results.html')) document.location.href = 'results.html'
+          else if (typeof setCity !== 'undefined') setCity(costData.city_id, true)
         }
       })
   },
@@ -114,28 +118,54 @@ const GeoApi = {
     for (let city of cachedResults) {
       this.renderCollectionItem(city)
     }
-  }
-}
+    if (document.location.href.includes('results.html')) {
+      let active = GeoApi.collectionEl.children('.collection-item').first().addClass('active')
+      $('#search').val(active.text())
+    }
 
-/**
- * EVENT LISTENERS FOR CITY SEARCH BAR AND FORM SUBMISSION
- */
-$('#search').on('keyup', function () { GeoApi.searchTerm = this.value })
-  .on('blur', function () {
-    /** Hides the Results Panel when the user clicks out of the Search bar */
-    GeoApi.resultsEl.fadeOut()
-  })
-  .on('focus', function () {
-    /** Shows the Results Panel when user clicks on the Search bar, only if there are items in the collection */
-    if (GeoApi.collectionEl.find('.collection-item').length) GeoApi.resultsEl.fadeIn()
-  })
-$('#searchByName').find('form').on('submit', GeoApi.handleSearch)
+  },
+  get currentLocation() {
+    let stored = localStorage.getItem('currentLocation')
+    return stored ? JSON.parse(stored) : null
+  },
+  renderCurrentLocationMap: function (map) {
+    if (navigator.geolocation) {
+      if (GeoApi.currentLocation) {
+        MapApi.renderMap(document.getElementById('searchByMap'), GeoApi.currentLocation, { position: GeoApi.currentLocation, title: 'Your Location' })
+        MapApi.renderNearbyCities()
+      } else {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+            localStorage.setItem('currentLocation', JSON.stringify(pos))
+            MapApi.renderMap(document.getElementById('searchByMap'), GeoApi.currentLocation, { position: GeoApi.currentLocation, title: 'Your Location' })
+            MapApi.renderNearbyCities()
+          },
+          () => {
+            console.log('error')
+          }
+        );
+      }
+    } else {
+      // Browser doesn't support Geolocation
+      console.error({ error: `Browser doesn't support Geolocation` })
+    }
+
+  },
+  // get nearbyCities() {
+  //   console.log(CostApi.cityList)
+  // }
+}
 
 /**
  * Defines an API that retrieves prices for more than 60 goods and services for more than 8000 cities
  * https://rapidapi.com/traveltables/api/cost-of-living-and-prices/
  */
 const CostApi = {
+  loading: true,
   fetchOptions: {
     method: 'GET',
     headers: {
@@ -188,6 +218,22 @@ const CostApi = {
       })
     }
   },
+  cityList: [],
+  fetchCityList: async function () {
+    let storedList = localStorage.getItem('CostApiCities')
+    if (storedList) this.cityList = JSON.parse(storedList)
+    else {
+      try {
+        const response = await fetch('https://cost-of-living-and-prices.p.rapidapi.com/cities', this.fetchOptions)
+        const data = await response.json()
+        if (data.error) throw data
+        this.cityList = data.cities
+        localStorage.setItem('CostApiCities', JSON.stringify(this.cityList))
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
   get cache() {
     let stored = localStorage.getItem('CostApiCache')
     if (!stored) return []
@@ -205,20 +251,148 @@ const CostApi = {
     let stored = this.cache
     return stored.find(cached => cached.city_id == city_id)
   },
-  renderCityCosts: function (costData) {
+  renderCityCosts: function (city) {
     /** Render elements related to city
      * Display main demographic data 
      * Display 'category' or 'goods' cards as well
      */
-    // console.log(this)
-    if (!costData) costData = this.currentCity
-    let geoData = GeoApi.findById(costData.geo_id)
-    console.log('Available For Rendering', { costData, geoData })
-    console.log('Sample Price Data', costData.prices[0])
-    console.log('Sample Category', this.categories[0])
+    if (city) {
+      this.renderAllCarousels(this.getCostsByCategory(city))
+    } else {
+      this.renderAllCarousels()
+    }
+  },
+  getCostsByCategory: function (city) {
+    if (!city) city = this.currentCity
+    const costs = {}
+    const { prices } = city
+    for (let price of prices) {
+      if (!costs[price.category_name]) costs[price.category_name] = []
+      costs[price.category_name].push(price)
+    }
+    return costs
+  },
+  formatCost: function (cost, measure, currency, usd) {
+    if (measure === 'currency' || measure === 'money') {
+      let value = new Intl.NumberFormat('en-US', { style: 'currency', currency }).format(cost)
+      if (currency === 'USD' || !usd) return value
+      else return value + ' (' + new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(usd) + ')'
+    } else if (measure === 'money') return new Intl.NumberFormat('en-US', { style: 'currency', currency }).format(cost)
+    else if (measure === 'percent') return new Intl.NumberFormat('en-US', { style: 'percent', minimumFractionDigits: 2 }).format(cost / 100)
+    else throw { error: 'measure not found', measure }
+  },
+  renderCarousel: function (prices, category_name, options = {}) {
+    const colors = ['red', 'blue', 'amber', 'green']
+    const carouselContainerEl = $('.carousel-info')
+    const getColor = (el) => {
+      let carouselIndex = $('.carousel-info').children().length, itemIndex = el.children().length
+      return colors[(carouselIndex + itemIndex) % colors.length]
+    }
+    // const format = new Intl.NumberFormat({style:'currency',currency})
+    // const formatCost = (cost, measure, currency = 'USD') => {
+    //   console.log(currency)
+    //   return new Intl.NumberFormat('en-US', { style: 'currency', currency }).format(cost)
+    // }
+    if (!prices) {
+      this.renderAllCarousels()
+    } else {
+      const carouselEl = $(`<div id="${category_name.replace(/[^a-zA-Z]/g, '')}" class="carousel carousel-slider center"></div>`).appendTo(carouselContainerEl)
+      for (let item of prices) {
+        let { item_name, avg, min, max, usd = {}, measure, currency_code } = item
+        carouselEl.append(`<div class="carousel-item ${getColor(carouselEl)} white-text" href="#one!">
+          <h2>${category_name}</h2>
+          <h4>${item_name}</h4>
+          <p class="white-text">Currency: ${currency_code}</p>
+          <p class="white-text">Average: ${CostApi.formatCost(avg, measure, currency_code, usd.avg)}</p>
+          <p class="white-text">Minimum: ${CostApi.formatCost(min, measure, currency_code, usd.min)}</p>
+          <p class="white-text">Maximum: ${CostApi.formatCost(max, measure, currency_code, usd.max)}</p>
+       `)
+      }
+    }
+  },
+  renderAllCarousels: function (categories) {
+    if (!categories) categories = this.getCostsByCategory()
+    $('.carousel-info').html('')
+
+    for (let [name, items] of Object.entries(categories)) {
+      this.renderCarousel(items, name)
+    }
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    });
   }
 }
 
+const MapApi = {
+  apiKey: "AIzaSyBgXZVqpjEkbxWRkmEw4ukpqDjNFGYAxo0",
+  mapEl: document.getElementById('CityMap'),
+  map: null,
+  markers: [],
+  renderMap: function (mapEl, center, markers = []) {
+    if (typeof google === 'undefined') return
+    // console.trace()
+    // console.log(mapEl, center, markers)
+    MapApi.mapEl = mapEl
+    MapApi.map = new google.maps.Map(MapApi.mapEl, {
+      zoom: 5,
+      center
+    });
+    if (!Array.isArray(markers)) markers = [markers]
+    for (let marker of markers) {
+      MapApi.createMarker(marker)
+    }
+  },
+  createMarker: function ({ position: { lat, lng }, ...options }) {
+    let marker = new google.maps.Marker({
+      position: { lat, lng },
+      map: MapApi.map,
+      animation: google.maps.Animation.DROP,
+      ...options
+    })
+    marker.addListener('click', () => {
+      console.log(marker)
+    })
+    MapApi.markers.unshift(marker)
+  },
+  renderNearbyCities: async function () {
+    const bounds = MapApi.map.getBounds()
+    if (!bounds) {
+      setTimeout(MapApi.renderNearbyCities, 500)
+    } else {
+      const cities = CostApi.cityList.filter(({ lat, lng }) => bounds.contains({ lat, lng }))
+      for (let i = 0; i < 5; i++) {
+        // for (let city of cities) {
+        setTimeout(() => {
+          console.log(i)
+          const { lat, lng, city_name, country_name } = cities.splice(Math.floor(Math.random() * cities.length), 1)[0]
+          MapApi.createMarker({ position: { lat, lng }, title: `${city_name},${country_name}` })
+        }, i * 200)
+      }
+      console.log(bounds)
+    }
+
+  }
+}
+
+function getCombinedCityData() {
+  return { ...CostApi.currentCity, geo: GeoApi.currentCity }
+}
+
+/**
+ * EVENT LISTENERS FOR CITY SEARCH BAR AND FORM SUBMISSION
+ */
+$('#search').on('keyup', function () { GeoApi.searchTerm = this.value })
+  .on('blur', function () {
+    /** Hides the Results Panel when the user clicks out of the Search bar, only when not in an `aside` */
+    if ($(this).parents('aside').length === 0) GeoApi.resultsEl.fadeOut()
+  })
+  .on('focus', function () {
+    /** Shows the Results Panel when user clicks on the Search bar, only if there are items in the collection */
+    if (GeoApi.collectionEl.find('.collection-item').length) GeoApi.resultsEl.fadeIn()
+  })
+$('#searchByName').find('form').on('submit', GeoApi.handleSearch)
+$('a[href="#searchByMap"]').on('click', () => { GeoApi.renderCurrentLocationMap() })
 
 /**
  * Not intended for production, just a demonstration of the API
@@ -248,7 +422,7 @@ async function demo() {
   else CostApi.renderCityCosts()
 }
 
-
+window.mapInit = () => { }
 // RUN DEMO
 // demo()
 
--- assets/js/results.js
+++ assets/js/results.js
@@ -1,23 +1,40 @@
 
 $(document).ready(function () {
   const stored_id = localStorage.getItem('city_id')
-  if (stored_id) {
-    CostApi.currentCity = CostApi.findById(stored_id)
-    GeoApi.currentCity = GeoApi.findById(CostApi.currentCity.geo_id)
-    loadCityInformation(getCombinedCityData())
-  }  
+  if (stored_id) setCity(stored_id)
 })
 
-function getCombinedCityData() {
-  return { ...CostApi.currentCity, geo: GeoApi.currentCity }
+function setCity(city_id, renderCachedList = false) {
+  CostApi.currentCity = CostApi.findById(city_id)
+  GeoApi.currentCity = GeoApi.findById(CostApi.currentCity.geo_id)
+  loadCityInformation(getCombinedCityData())
+  if (renderCachedList) GeoApi.renderCachedResults()
 }
 
+
+
 function loadCityInformation(city) {
-  let { city_name, country_name, geo } = city
+  $('#ResultsHeader').text(city.city_name)
+  renderDemographics(city)
+  console.log(city)
+  // const city = getCombinedCityData()
+  const { latitude: lat, longitude: lng } = city.geo || { latitude: 0, longitude: 0 }
+  const marker = {position:{ lat, lng }, title: `${city.city_name}, ${city.geo.region}, ${city.country_name}` }
+
+
+  MapApi.renderMap(document.getElementById('CityMap'), { lat, lng }, [marker])
+  CostApi.renderCityCosts(city)
+}
+
+function renderDemographics(city) {
+  let { city_name, country_name, exchange_rate, exchange_rates_updated: updated_on, geo } = city
   let { latitude, longitude, region, population } = geo
-
-  $('#ResultsHeader').text(city_name)
-  let dataEl = $('#CityData')
-
+  $('#Demographics').html(`
+    <h2>${city_name}</h2>
+    <h3>${region}, ${country_name}</h3>
+    <p><b>Population</b>: ${new Intl.NumberFormat('en-US').format(population)}</p>
+    <p><b>Location</b>: (${latitude},${longitude})</p>
+    <p><b>Exchange rates</b>: ${Object.entries(exchange_rate).map(([cur, val]) => `${cur}: ${CostApi.formatCost(val, 'currency', cur)}`).join(', ')}</p>
+  `)
 }
 
