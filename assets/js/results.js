44d0bf823f056e35a9defeda537d015579e287e4
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-07-13 03:47:37 -0500

    i need sleep

--- assets/js/results.js
+++ assets/js/results.js
@@ -38,7 +38,9 @@ function loadCityInformation(city) {
 /** Uses the `GeoApi` city data to show demographic information and exchange rates */
 function renderCityInfo(city) {
   let { city_name, country_name, exchange_rate, exchange_rates_updated: updated_on, geo } = city
-  let { latitude, longitude, region, population,wikiDataId } = geo
+  let { latitude, longitude, region, population
+  , wikiDataId 
+  } = geo
   let currency = city.prices[0].currency_code
   const getRate = (cur) => CostApi.formatCost(exchange_rate[cur], 'currency', cur)
   const renderExchangeRate = (cur) => exchange_rate[cur] ? `<p><b>Exchange rate</b>: $1 USD = ${getRate(currency)}</p>` : ''
@@ -48,6 +50,6 @@ function renderCityInfo(city) {
     <p><b>Population</b>: ${new Intl.NumberFormat('en-US').format(population)}</p>
     <p><b>Location</b>: (${latitude},${longitude})</p>
     ${renderExchangeRate(currency)}
-    <iframe src='https://www.wikidata.org/wiki/Special:EntityData/${wikiDataId}' width='100%' ></iframe>
+    
   `)
 }
